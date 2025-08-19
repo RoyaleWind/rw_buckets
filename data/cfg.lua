70da0f6e8e2ee83ed186a2b464a9c8575a5f5e9a
RoyaleWind <61204500+RoyaleWind@users.noreply.github.com>
2025-08-19 11:41:44 +0300

    Update cfg.lua

--- data/cfg.lua
+++ data/cfg.lua
@@ -1,5 +1,7 @@
 return {
     useDataTables = true,
+    --- FIVEM VERSION https://store.royalewind.com/Product/6983657
+    --- REDM VERSION https://store.royalewind.com/Product/6954818
     -- Command settings
     commandCooldown = 2000, -- Cooldown time in milliseconds for bucket-related commands
 
